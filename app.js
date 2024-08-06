165911f207fa56573f7093b9ed2171b3e7bb8f9f
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-08-06 21:50:14 +0200

    any

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
+  app.onAny(async (context) => {
+    app.log.info({ context });
+  });
+
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
