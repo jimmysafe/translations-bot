6c2b9efc9ed4e1501248c52f9ba6d03f42d2cb5d
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-08-06 21:24:56 +0200

    issues

--- app.js
+++ app.js
@@ -4,12 +4,9 @@
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
-  app.on(
-    ["pull_request.opened", "pull_request.synchronize", "issues.opened"],
-    async (context) => {
-      return context.octokit.issues.createComment(
-        context.issue({ body: "Hello, World!" })
-      );
-    }
-  );
+  app.on("issues.opened", async (context) => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Hello, World!" })
+    );
+  });
 };
