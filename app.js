ab7c946ebca91606604f4bbba4227269998adc92
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-08-07 11:32:56 +0200

    feat: logs

--- app.js
+++ app.js
@@ -4,13 +4,16 @@
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
-  app.onAny(async (context) => {
-    app.log.info({ context });
-  });
-
   app.on("issues.opened", async (context) => {
-    return context.octokit.issues.createComment(
+    const res = await context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
+      // creo task
+      // task creato -> aggiorno commento
     );
+    console.log(res);
+    return res;
   });
 };
+
+// On open PR -> crea commento con tabella. in META ID COMMENTO.
+// on workflow complete -> CREA TASK SU LOKALISE -> AGGIORNA IL COMMENTO.
