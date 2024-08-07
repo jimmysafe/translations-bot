46f24f39256bd7a34d27ba727c81bff2062f6156
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-08-07 13:03:31 +0200

    feat: func2

--- api/lokalise/webhooks/task-closed/index.js
+++ api/lokalise/webhooks/task-closed/index.js
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.json({ message: "Method Not Allowed" });
-  }
-  return res.json({
-    message: `Hello World!`,
-  });
+  console.log(res);
 }
